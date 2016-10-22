77f0903ff5436b9da6c90ebf70dc8f2d194e8851
leadenmoth <astashenkovaa@gmail.com>
2016-10-22 20:29:49 +0300

    First pass at testing. Smoke works, JPA doesn't. Started adding find methods to repositories

--- src/main/java/en/haagahelia/course/domain/BookRepository.java
+++ src/main/java/en/haagahelia/course/domain/BookRepository.java
@@ -1,7 +1,11 @@
 package en.haagahelia.course.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+import en.haagahelia.course.domain.Book;
 
+public interface BookRepository extends CrudRepository<Book, Long> {
+	List<Book> findByAuthor(String author);
 }
---
+++ src/test/java/en/haagahelia/course/BookRepositoryTest.java
@@ -0,0 +1,51 @@
+package en.haagahelia.course;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import en.haagahelia.course.domain.Category;
+import en.haagahelia.course.domain.Book;
+import en.haagahelia.course.domain.BookRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository repository;
+   
+    @Test
+    public void findByAuthorShouldReturnBook() {
+        List<Book> books = repository.findByAuthor("Eoin Colfer");
+        
+        assertThat(books).hasSize(1);
+        assertThat(books.get(0).getTitle()).isEqualTo("The Hitchhiker's Guide to the Galaxy");
+    }
+    
+    @Test
+    public void createNewBook() {
+    	Book book = new Book("Mickey Mouse", "Walt Disney", 1928, "98764325405", 0.99, new Category("Script"));
+    	repository.save(book);
+    	assertThat(book.getId()).isNotNull();
+    } 
+    
+
+    
+    @Test
+    public void deleteBook() {
+        List<Book> books = repository.findByAuthor("Douglas Adams");
+        repository.delete(books.get(0).getId());
+        assertThat(repository.findOne(books.get(0).getId()) == null);
+        
+    }
+    
+   
+
+}
